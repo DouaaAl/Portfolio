622c4228985ffba0bbe37e3b28d6a5d7eea3704e
DouaaAl <elmahraouidouaa123@gmail.com>
2024-09-06 10:03:39 +0100

    portfolio

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from 'react';
+import React from 'react';
 import Image from 'next/image';
 import Styles from "../styles/main.module.css";
 import Contact from './contact';
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import Face from "../../../public/face.jpg";
 import Linkdin from "../../../public/icons/LinkedIn.png";
 import Email from "../../../public/icons/Email.png";
